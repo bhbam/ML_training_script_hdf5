3a6f1e9028201f4bc7a106f95a3494799fc093a3
bhbam <bbbam@crimson.ua.edu>
2026-04-15 21:23:48 -0500

    miniAod and AOD test

--- mass_trainer_with_ieta_iphi_separete_train_valid_set_with_channel_select.sh
+++ mass_trainer_with_ieta_iphi_separete_train_valid_set_with_channel_select.sh
@@ -6,8 +6,8 @@
 #SBATCH --gres=gpu:1                                # Allocate GPU
 #SBATCH -N 1                                        # only use 1 node — in this case, 16 cores on 1 node
 #SBATCH --mem-per-cpu=7G                           # reserve 6G of memory
-#SBATCH -J sl_12ch             # job name
-#SBATCH -o sl_12ch.%A
+#SBATCH -J AOD_5ch             # job name
+#SBATCH -o AOD_5ch.%A
 #SBATCH --mail-user=bbbam@crimson.ua.edu
 
 source /share/apps/modulefiles/conda_init.sh
